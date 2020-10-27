24263d9b597756e71daa8917d5dbb639baadd593
Dup4 <38343778+Dup4@users.noreply.github.com>
2020-10-27 12:53:59 +0800

    undone html-mini

--- config/config.ts
+++ config/config.ts
@@ -1,7 +1,24 @@
 import { defineConfig } from 'umi';
 import CONFIG from '../config';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 export default defineConfig({
+    // chainWebpack(config, { webpack }) {
+    //     config
+    //         .plugin('html')
+    //         .use(HtmlWebpackPlugin, [{
+    //             template: '@/pages/document.ejs',
+    //             filename: 'index.html',
+    //             minify: {
+    //                 collapseWhitespace: true,
+    //                 removeComments: true,
+    //                 removeRedundantAttributes: true,
+    //                 removeScriptTypeAttributes: true,
+    //                 removeStyleLinkTypeAttributes: true,
+    //                 useShortDoctype: true,
+    //             },
+    //         }]);
+    // },
     title: CONFIG.title,
     nodeModulesTransform: {
         type: 'none',
