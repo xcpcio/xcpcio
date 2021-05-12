97bcd3336670e8e754f6aef63a1977cd0e019194
Dup4 <lyuzhi.pan@gmail.com>
2021-05-13 00:30:48 +0800

    style: format

--- .umirc.ts
+++ .umirc.ts
@@ -3,32 +3,32 @@ import CONFIG from './config';
 import CaseSensitivePathsWabpackPlugin from 'case-sensitive-paths-webpack-plugin';
 
 export default defineConfig({
-    chainWebpack(config, { webpack }) {
-        // 检查路径大小写
-        config
-            .plugin('case-sensitive-paths-webpack-plugin')
-            .use(CaseSensitivePathsWabpackPlugin, [{ debug: false }]);
+  chainWebpack(config, { webpack }) {
+    // 检查路径大小写
+    config
+      .plugin('case-sensitive-paths-webpack-plugin')
+      .use(CaseSensitivePathsWabpackPlugin, [{ debug: false }]);
+  },
+  title: false,
+  metas: CONFIG.metas,
+  favicon: CONFIG.favicon,
+  analytics: CONFIG.analytics,
+  nodeModulesTransform: {
+    type: 'none',
+  },
+  hash: true,
+  routes: [
+    {
+      path: '/',
+      component: '@/pages/index',
+      exact: true,
     },
-    title: false,
-    metas: CONFIG.metas,
-    favicon: CONFIG.favicon,
-    analytics: CONFIG.analytics,
-    nodeModulesTransform: {
-        type: 'none',
+    {
+      path: '/',
+      component: '@/pages/board/board',
+      exact: false,
     },
-    hash: true,
-    routes: [
-        {
-            path: '/',
-            component: '@/pages/index',
-            exact: true,
-        },
-        {
-            path: '/',
-            component: '@/pages/board/board',
-            exact: false,
-        },
-    ],
-    proxy: CONFIG.proxy,
-    publicPath: CONFIG.publicPath,
+  ],
+  proxy: CONFIG.proxy,
+  publicPath: CONFIG.publicPath,
 });
