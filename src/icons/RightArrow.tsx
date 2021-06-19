c4ed967f27ea889b65e204dc52eb332b27704952
Dup4 <lyuzhi.pan@gmail.com>
2021-06-20 02:35:32 +0800

    refactor: icon

--- src/icons/RightArrow.tsx
+++ src/icons/RightArrow.tsx
@@ -1,36 +1,31 @@
-import React from 'react';
+import React from "react";
 
-class RightArrowIcon extends React.Component {
-  constructor(props: any) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <svg
-        stroke="currentColor"
-        fill="currentColor"
-        strokeWidth="0"
-        viewBox="0 0 1024 1024"
-        version="1.1"
-        xmlns="http://www.w3.org/2000/svg"
-        p-id="1161"
-        width="30"
-        height="30"
-      >
-        <path
-          d="M837.9904 570.0608H124.5696a29.0304 29.0304 0 0 1-29.0304-29.0304V482.9696a29.0304 29.0304 0 0 1 29.0304-29.0304h713.4208z"
-          fill="#2C2C2C"
-          p-id="1162"
-        ></path>
-        <path
-          d="M561.3056 808.96l-40.96-40.96a28.928 28.928 0 0 1 0-40.96l215.04-215.04-215.04-215.04a28.9792 28.9792 0 0 1 0-40.96l40.96-40.96a28.9792 28.9792 0 0 1 40.96 0l296.96 296.96-296.96 296.96a28.9792 28.9792 0 0 1-40.96 0z"
-          fill="#2C2C2C"
-          p-id="1163"
-        ></path>
-      </svg>
-    );
-  }
-}
+const RightArrowIcon: React.FC<{}> = () => {
+  return (
+    <svg
+      stroke="currentColor"
+      fill="currentColor"
+      strokeWidth="0"
+      viewBox="0 0 1024 1024"
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+      p-id="1161"
+      width="30"
+      height="30"
+    >
+      <path
+        d="M837.9904 570.0608H124.5696a29.0304 29.0304 0 0 1-29.0304-29.0304V482.9696a29.0304 29.0304 0 0 1 29.0304-29.0304h713.4208z"
+        fill="#2C2C2C"
+        p-id="1162"
+      ></path>
+      <path
+        d="M561.3056 808.96l-40.96-40.96a28.928 28.928 0 0 1 0-40.96l215.04-215.04-215.04-215.04a28.9792 28.9792 0 0 1 0-40.96l40.96-40.96a28.9792 28.9792 0 0 1 40.96 0l296.96 296.96-296.96 296.96a28.9792 28.9792 0 0 1-40.96 0z"
+        fill="#2C2C2C"
+        p-id="1163"
+      ></path>
+    </svg>
+  );
+};
 
 export { RightArrowIcon };
+export default RightArrowIcon;
