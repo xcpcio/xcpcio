04b2e5567bbdfb6d759e95bda8fbc7b75f8295ea
Dup4 <lyuzhi.pan@gmail.com>
2021-06-22 14:01:02 +0800

    refactor: loading

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import style from './Loading.less';
+import React from "react";
+import style from "./Loading.module.less";
 
-class Loading extends React.Component {
-  constructor(props: any) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div className={[style['loader21'], style['loader']].join(' ')}>
-        <div className={style['loader-21']}>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-        </div>
+const Loading: React.FC<{}> = () => {
+  return (
+    <div className={[style["loader21"], style["loader"]].join(" ")}>
+      <div className={style["loader-21"]}>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export { Loading };
