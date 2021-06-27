cc0a90e12218e9426a4b19332ae7bf7d707a802d
Dup4 <lyuzhi.pan@gmail.com>
2021-06-27 21:16:28 +0800

    refactor: Balloon

--- src/components/Balloon/Balloon.tsx
+++ src/components/Balloon/Balloon.tsx
@@ -3,16 +3,7 @@ import style from "./Balloon.module.less";
 import Highlighter from "react-highlight-words";
 import React from "react";
 import { SearchOutlined } from "@ant-design/icons";
-import {
-  Layout,
-  Table,
-  Input,
-  Space,
-  Button,
-  Skeleton,
-  Popconfirm,
-  Tooltip,
-} from "antd";
+import { Table, Input, Space, Button } from "antd";
 import { deepCopy } from "@/utils";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -23,6 +14,8 @@ import {
   faUndoAlt,
 } from "@fortawesome/free-solid-svg-icons";
 import { debounce, throttle } from "lodash";
+import { Run } from "@/interface/submission";
+import { isAccepted } from "@/utils/submission";
 
 interface ActionItem {
   status: boolean;
@@ -278,53 +271,75 @@ class Balloon extends React.Component {
     return columns;
   }
 
-  getTableData(contest_config: any, team: any, run: any) {
+  getTableData(contest_config: any, team: any, run: Run[]) {
     if (contest_config?.balloon_color == null) return [];
     let tableData: BalloonTableItem[] = [];
+
     for (let k in team) {
       team[k]["total"] = [];
     }
+
     const balloon_dispatched_set = new Set(
       JSON.parse(
         window.localStorage.getItem(getBalloonDispatchedListKey()) || "[]",
       ),
     );
+
+    const uniqueTeamProblemSolved = new Set();
+
     let first_solved = new Set();
+
     run
-      .filter((x) => x.status === "correct")
-      .forEach((run: any) => {
-        team[run.team_id].total.push(run.problem_id);
-        team[run.team_id].total.sort((a: number, b: number) => a - b);
-        let awards = "";
-        if (!first_solved.has(run.problem_id)) {
-          awards = `First to solved problem ${
-            contest_config["problem_id"][run.problem_id]
-          }`;
-          first_solved.add(run.problem_id);
+      .filter((x) => isAccepted(x.status))
+      .forEach((run: Run) => {
+        if (
+          uniqueTeamProblemSolved.has(getSolvedId(run.teamId, run.problemId))
+        ) {
+          return;
         }
+
+        uniqueTeamProblemSolved.add(getSolvedId(run.teamId, run.problemId));
+
+        team[run.teamId].total.push(run.problemId);
+        team[run.teamId].total.sort((a: number, b: number) => a - b);
+
+        let awards = "";
+
+        if (!first_solved.has(run.problemId)) {
+          awards = `First to solved problem ${
+            contest_config["problem_id"][run.problemId]
+          }`;
+          first_solved.add(run.problemId);
+        }
+
         const status = balloon_dispatched_set.has(
-          getSolvedId(run.team_id, run.problem_id),
+          getSolvedId(run.teamId, run.problemId),
         );
+
         let tableItem: BalloonTableItem = {
           status: status,
           time: run.timestamp,
-          solved: run.problem_id as number,
-          total: deepCopy(team[run.team_id].total),
+          solved: run.problemId,
+          total: deepCopy(team[run.teamId].total),
           action: {
             status: status,
-            solvedId: getSolvedId(run.team_id, run.problem_id),
+            solvedId: getSolvedId(run.teamId, run.problemId),
           },
-          team: team[run.team_id].name,
+          team: team[run.teamId].name,
           awards: awards,
         };
+
         if (contest_config?.badge) {
-          tableItem.badge = team[run.team_id].badge?.base64;
+          tableItem.badge = team[run.teamId].badge?.base64;
         }
+
         if (contest_config?.organization) {
-          tableItem.organization = team[run.team_id].organization;
+          tableItem.organization = team[run.teamId].organization;
         }
+
         tableData.push(tableItem);
       });
+
     tableData.sort((a: BalloonTableItem, b: BalloonTableItem) => {
       if (a.status && !b.status) return 1;
       if (!a.status && b.status) return -1;
@@ -332,6 +347,7 @@ class Balloon extends React.Component {
       if (a.time > b.time) return -1;
       return 0;
     });
+
     return tableData;
   }
 
@@ -341,7 +357,9 @@ class Balloon extends React.Component {
       props.team,
       props.run,
     );
+
     const columns = this.getColumns(props.contest_config);
+
     this.setState({
       loaded: true,
       on: props.contest_config?.balloon_color ? true : false,
@@ -362,7 +380,6 @@ class Balloon extends React.Component {
     debounce(this.update, 100).bind(this)(nextProps);
   }
 
-  //组件卸载前的操作
   componentWillUnmount() {}
 
   state = {
