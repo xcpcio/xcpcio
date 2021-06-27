ce94da63e8c3b5acede37f0d4e88027e730c007d
Dup4 <lyuzhi.pan@gmail.com>
2021-06-27 16:31:49 +0800

    feat: submission status judge

--- src/interface/submission.ts
+++ src/interface/submission.ts
@@ -1,73 +1,65 @@
 export enum SubmissionStatus {
-  Accepted = 'Accepted',
-  Correct = 'Correct',
-  correct = 'Correct',
+  Pending = "Pending",
+  Frozen = "Frozen",
 
-  Pending = 'Pending',
+  Accepted = "Accepted",
 
-  ConfigurationError = 'Configuration Error',
-  SystemError = 'System Error',
-  Canceled = 'Canceled',
+  CompilationError = "Compilation Error",
+  PresentationError = "Presentation Error",
 
-  CompilationError = 'Compilation Error',
+  FileError = "File Error",
+  RuntimeError = "Runtime Error",
 
-  FileError = 'File Error',
-  RuntimeError = 'Runtime Error',
-  TimeLimitExceeded = 'Time Limit Exceeded',
-  MemoryLimitExceeded = 'Memory Limit Exceeded',
-  OutputLimitExceeded = 'Output Limit Exceeded',
+  TimeLimitExceeded = "Time Limit Exceeded",
+  MemoryLimitExceeded = "Memory Limit Exceeded",
+  OutputLimitExceeded = "Output Limit Exceeded",
+  IdlenessLimitExceeded = "Idleness LimitExceeded",
 
-  PartiallyCorrect = 'Partially Correct',
-  WrongAnswer = 'Wrong Answer',
-  Reject = 'Reject',
-  InCorrect = 'InCorrect',
-  incorrect = 'Incorrect',
+  WrongAnswer = "Wrong Answer",
+  Reject = "Reject",
 
-  JudgementFailed = 'Judgement Failed',
+  JudgementFailed = "Judgement Failed",
 
-  Frozen = 'Frozen',
-  Hacked = ' Hacked',
+  Hacked = "Hacked",
 
-  Skipped = 'Skipped',
-  PresentationError = 'Presentation Error',
+  ConfigurationError = "Configuration Error",
+  SystemError = "System Error",
+  Canceled = "Canceled",
+  Skipped = "Skipped",
 
-  SecurityViolated = 'Security Violated',
-  DenialOfJudgement = 'Denial Of Judgement',
-  IdlenessLimitExceeded = 'Idleness LimitExceeded',
+  SecurityViolated = "Security Violated",
+  DenialOfJudgement = "Denial Of Judgement",
+
+  PartiallyCorrect = "Partially Correct",
 }
 
 export type RunStatus =
-  | 'Accepted'
-  | 'Correct'
-  | 'correct'
-  | 'Pending'
-  | 'pending'
-  | 'ConfigurationError'
-  | 'SystemError'
-  | 'Canceled'
-  | 'CompilationError'
-  | 'FileError'
-  | 'RuntimeError'
-  | 'TimeLimitExceeded'
-  | 'MemoryLimitExceeded'
-  | 'OutputLimitExceeded'
-  | 'PartiallyCorrect'
-  | 'WrongAnswer'
-  | 'Reject'
-  | 'InCorrect'
-  | 'incorrect'
-  | 'JudgementFailed'
-  | 'Frozen'
-  | 'Hacked'
-  | 'Skipped'
-  | 'PresentationError'
-  | 'SecurityViolated'
-  | 'DenialOfJudgement'
-  | 'IdlenessLimitExceeded';
+  | "Accepted"
+  | "Pending"
+  | "ConfigurationError"
+  | "SystemError"
+  | "Canceled"
+  | "CompilationError"
+  | "FileError"
+  | "RuntimeError"
+  | "TimeLimitExceeded"
+  | "MemoryLimitExceeded"
+  | "OutputLimitExceeded"
+  | "PartiallyCorrect"
+  | "WrongAnswer"
+  | "Reject"
+  | "JudgementFailed"
+  | "Frozen"
+  | "Hacked"
+  | "Skipped"
+  | "PresentationError"
+  | "SecurityViolated"
+  | "DenialOfJudgement"
+  | "IdlenessLimitExceeded";
 
 export interface Run {
   team_id: number | string;
   timestamp: number;
   problem_id: number;
-  status: RunStatus;
+  status: SubmissionStatus;
 }
--- src/utils/submission.ts
+++ src/utils/submission.ts
@@ -1,26 +1,50 @@
-import { RunStatus } from '@/interface/submission';
+import { SubmissionStatus } from "@/interface/submission";
 
-export function isAccepted(status: RunStatus): boolean {
-  if (status === 'Accepted' || status === 'Correct' || status === 'correct') {
+export function isAccepted(status: SubmissionStatus): boolean {
+  if (status === SubmissionStatus.Accepted) {
     return true;
   }
   return false;
 }
 
-export function isWrongAnswer(status: RunStatus): boolean {
+export function isWrongAnswer(status: SubmissionStatus): boolean {
   if (
-    status === 'WrongAnswer' ||
-    status === 'Reject' ||
-    status === 'InCorrect' ||
-    status === 'incorrect'
+    status === SubmissionStatus.WrongAnswer ||
+    status === SubmissionStatus.RuntimeError ||
+    status === SubmissionStatus.TimeLimitExceeded ||
+    status === SubmissionStatus.MemoryLimitExceeded ||
+    status === SubmissionStatus.OutputLimitExceeded ||
+    status === SubmissionStatus.IdlenessLimitExceeded ||
+    status === SubmissionStatus.Reject ||
+    status === SubmissionStatus.JudgementFailed ||
+    status === SubmissionStatus.Hacked
   ) {
     return true;
   }
   return false;
 }
 
-export function isPending(status: RunStatus): boolean {
-  if (status === 'Pending' || status === 'pending' || status === 'Frozen') {
+export function isPending(status: SubmissionStatus): boolean {
+  if (
+    status === SubmissionStatus.Pending ||
+    status === SubmissionStatus.Frozen
+  ) {
+    return true;
+  }
+  return false;
+}
+
+export function isNotCalculatedPenaltyStatus(
+  status: SubmissionStatus,
+): boolean {
+  if (
+    status === SubmissionStatus.CompilationError ||
+    status === SubmissionStatus.PresentationError ||
+    status === SubmissionStatus.ConfigurationError ||
+    status === SubmissionStatus.SystemError ||
+    status === SubmissionStatus.Canceled ||
+    status === SubmissionStatus.Skipped
+  ) {
     return true;
   }
   return false;
