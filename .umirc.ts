3f2982e5551c1e1ab12b3db137a820fa8b8a4b8a
Dup4 <lyuzhi.pan@gmail.com>
2021-12-06 09:47:02 +0800

    chore: upgrade umi

--- .umirc.ts
+++ .umirc.ts
@@ -1,27 +1,27 @@
-import { defineConfig } from "umi";
-import __CONFIG__ from "./config";
+import { defineConfig } from 'umi';
+import __CONFIG__ from './config';
 
 export default defineConfig({
   title: false,
   metas: __CONFIG__.metas,
   favicon: __CONFIG__.favicon,
   analytics: __CONFIG__.analytics,
+  proxy: __CONFIG__.proxy,
+  publicPath: __CONFIG__.publicPath,
   nodeModulesTransform: {
-    type: "none",
+    type: 'none',
   },
   hash: true,
   routes: [
     {
-      path: "/",
-      component: "@/pages/index",
+      path: '/',
+      component: '@/pages/index',
       exact: true,
     },
     {
-      path: "/",
-      component: "@/pages/board/board",
+      path: '/',
+      component: '@/pages/board/board',
       exact: false,
     },
   ],
-  proxy: __CONFIG__.proxy,
-  publicPath: __CONFIG__.publicPath,
 });
