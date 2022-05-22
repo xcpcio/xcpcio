6e2343b6ae75ea57618ab2dc98dceff5599b6316
Dup4 <lyuzhi.pan@gmail.com>
2022-05-23 00:13:18 +0800

    fix: replace with all *.html

--- docker/docker_entry.sh
+++ docker/docker_entry.sh
@@ -5,28 +5,27 @@ set -e -x
 BOARD_PATH="/app/board/dist"
 EXPORT_PATH="/app/export"
 EXPORT_BOARD_PATH="${EXPORT_PATH}/board"
-INDEX_HTML_PATH="/app/board/dist/index.html"
 
 if [ -z "${PUBLIC_PATH}" ]; then
     PUBLIC_PATH=""
 fi
 
-sed -i "s|__PUBLIC_PATH__|${PUBLIC_PATH}|g" "${INDEX_HTML_PATH}"
+sed -i "s|__PUBLIC_PATH__|${PUBLIC_PATH}|g" /app/board/dist/*.html
 
 if [ -n "${DATA_HOST}" ]; then
-    sed -i "s|__DATA_HOST__|${DATA_HOST}|g" "${INDEX_HTML_PATH}"
+    sed -i "s|__DATA_HOST__|${DATA_HOST}|g" /app/board/dist/*.html
 fi
 
 if [ -n "${DEFAULT_TITLE}" ]; then
-    sed -i "s|__DEFAULT_TITLE__|${DEFAULT_TITLE}|g" "${INDEX_HTML_PATH}"
+    sed -i "s|__DEFAULT_TITLE__|${DEFAULT_TITLE}|g" /app/board/dist/*.html
 fi
 
 if [ -n "${BAIDU_ANALYTICS_ID}" ]; then
-    sed -i "s|__BAIDU_ANALYTICS_ID__|${BAIDU_ANALYTICS_ID}|g" "${INDEX_HTML_PATH}"
+    sed -i "s|__BAIDU_ANALYTICS_ID__|${BAIDU_ANALYTICS_ID}|g" /app/board/dist/*.html
 fi
 
 if [ -n "${GOOGLE_ANALYTICS_ID}" ]; then
-    sed -i "s|__GOOGLE_ANALYTICS_ID__|${GOOGLE_ANALYTICS_ID}|g" "${INDEX_HTML_PATH}"
+    sed -i "s|__GOOGLE_ANALYTICS_ID__|${GOOGLE_ANALYTICS_ID}|g" /app/board/dist/*.html
 fi
 
 if [ -d "${EXPORT_BOARD_PATH}" ]; then
