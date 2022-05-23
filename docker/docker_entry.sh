854d987755b54d4cda78a5f35e43fae0d7d50c75
Dup4 <lyuzhi.pan@gmail.com>
2022-05-23 10:13:15 +0800

    fix: add double quote when replace

--- docker/docker_entry.sh
+++ docker/docker_entry.sh
@@ -13,19 +13,19 @@ fi
 sed -i "s|__PUBLIC_PATH__|${PUBLIC_PATH}|g" /app/board/dist/*.html
 
 if [ -n "${DATA_HOST}" ]; then
-    sed -i "s|__DATA_HOST__|${DATA_HOST}|g" /app/board/dist/*.html
+    sed -i "s|__DATA_HOST__|\"${DATA_HOST}\"|g" /app/board/dist/*.html
 fi
 
 if [ -n "${DEFAULT_TITLE}" ]; then
-    sed -i "s|__DEFAULT_TITLE__|${DEFAULT_TITLE}|g" /app/board/dist/*.html
+    sed -i "s|__DEFAULT_TITLE__|\"${DEFAULT_TITLE}\"|g" /app/board/dist/*.html
 fi
 
 if [ -n "${BAIDU_ANALYTICS_ID}" ]; then
-    sed -i "s|__BAIDU_ANALYTICS_ID__|${BAIDU_ANALYTICS_ID}|g" /app/board/dist/*.html
+    sed -i "s|__BAIDU_ANALYTICS_ID__|\"${BAIDU_ANALYTICS_ID}\"|g" /app/board/dist/*.html
 fi
 
 if [ -n "${GOOGLE_ANALYTICS_ID}" ]; then
-    sed -i "s|__GOOGLE_ANALYTICS_ID__|${GOOGLE_ANALYTICS_ID}|g" /app/board/dist/*.html
+    sed -i "s|__GOOGLE_ANALYTICS_ID__|\"${GOOGLE_ANALYTICS_ID}\"|g" /app/board/dist/*.html
 fi
 
 if [ -d "${EXPORT_BOARD_PATH}" ]; then
