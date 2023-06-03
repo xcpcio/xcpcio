9967fe4d6227be57a4ffab98a8ec2b12ef6b59d3
Dup4 <lyuzhi.pan@gmail.com>
2023-06-03 19:21:19 +0800

    chore: fix Dockerfile
    Signed-off-by: Dup4 <lyuzhi.pan@gmail.com>

--- docker/docker_entry.sh
+++ docker/docker_entry.sh
@@ -1,39 +1,19 @@
-#! /bin/sh
+#! /bin/bash
 
-set -e -x
+CUR_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 
 BOARD_PATH="/app/board/dist"
 EXPORT_PATH="/app/export"
 EXPORT_BOARD_PATH="${EXPORT_PATH}/board"
 
-if [ -z "${PUBLIC_PATH}" ]; then
-    PUBLIC_PATH=""
-fi
+bash "${CUR_DIR}/scripts/inject_vars.sh" "${BOARD_PATH}/index.html"
 
-sed -i "s|__PUBLIC_PATH__|${PUBLIC_PATH}|g" /app/board/dist/*.html
-
-if [ -n "${DATA_HOST}" ]; then
-    sed -i "s|__DATA_HOST__|\"${DATA_HOST}\"|g" /app/board/dist/*.html
-fi
-
-if [ -n "${DEFAULT_TITLE}" ]; then
-    sed -i "s|__DEFAULT_TITLE__|\"${DEFAULT_TITLE}\"|g" /app/board/dist/*.html
-fi
-
-if [ -n "${BAIDU_ANALYTICS_ID}" ]; then
-    sed -i "s|__BAIDU_ANALYTICS_ID__|\"${BAIDU_ANALYTICS_ID}\"|g" /app/board/dist/*.html
-fi
-
-if [ -n "${GOOGLE_ANALYTICS_ID}" ]; then
-    sed -i "s|__GOOGLE_ANALYTICS_ID__|\"${GOOGLE_ANALYTICS_ID}\"|g" /app/board/dist/*.html
-fi
-
-if [ -d "${EXPORT_BOARD_PATH}" ]; then
+if [[ -d "${EXPORT_BOARD_PATH}" ]]; then
     cp -a "${BOARD_PATH}"/* "${EXPORT_BOARD_PATH}"/
 fi
 
-if [ X"${1}" = X"primary" ]; then
-    cd /app/board
+if [[ X"${1}" = X"primary" ]]; then
+    cd /app/board || exit 1
     exec npm run start
 else
     exec "${@}"
