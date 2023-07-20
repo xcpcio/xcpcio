0bf729a619e7f97442a542754cb995ab313b6ea9
Dup4 <lyuzhi.pan@gmail.com>
2023-07-20 09:59:36 +0800

    chore: docker
    Signed-off-by: Dup4 <lyuzhi.pan@gmail.com>

--- packages/apps/resolver/docker/docker_entry.sh
+++ packages/apps/resolver/docker/docker_entry.sh
@@ -5,8 +5,6 @@ CUR_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 BOARD_PATH="${CUR_DIR}/dist"
 EXPORT_PATH="/app/export"
 
-bash "${CUR_DIR}/../scripts/inject_vars.sh" "${BOARD_PATH}"/*.html
-
 if [[ -d "${EXPORT_PATH}" ]]; then
     cp -a "${BOARD_PATH}"/* "${EXPORT_PATH}"/
 fi
