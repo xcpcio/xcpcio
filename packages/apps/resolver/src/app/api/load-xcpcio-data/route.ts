6fd9b48ef2593af431360db6e2e263cb4b1ead9a
Dup4 <lyuzhi.pan@gmail.com>
2023-07-22 17:29:58 +0800

    feat: support resolver link in board
    Signed-off-by: Dup4 <lyuzhi.pan@gmail.com>

--- packages/apps/resolver/src/app/api/load-xcpcio-data/route.ts
+++ packages/apps/resolver/src/app/api/load-xcpcio-data/route.ts
@@ -2,14 +2,16 @@ import { NextRequest, NextResponse } from "next/server";
 
 import { IBoardData } from "@/lib/types";
 
+const DATA_HOST = "https://board.xcpcio.com/data";
+
 export async function GET(req: NextRequest) {
   const { searchParams } = new URL(req.url);
   const dataSource = searchParams.get("data-source") ?? "";
 
   const [configResp, teamResp, runResp] = await Promise.all([
-    fetch(`${dataSource}/config.json`, { cache: "no-store", mode: "no-cors" }),
-    fetch(`${dataSource}/team.json`, { cache: "no-store", mode: "no-cors" }),
-    fetch(`${dataSource}/run.json`, { cache: "no-store", mode: "no-cors" }),
+    fetch(`${DATA_HOST}/${dataSource}/config.json`, { cache: "no-store", mode: "no-cors" }),
+    fetch(`${DATA_HOST}/${dataSource}/team.json`, { cache: "no-store", mode: "no-cors" }),
+    fetch(`${DATA_HOST}/${dataSource}/run.json`, { cache: "no-store", mode: "no-cors" }),
   ]);
 
   if (configResp.status !== 200 || teamResp.status !== 200 || runResp.status !== 200) {
