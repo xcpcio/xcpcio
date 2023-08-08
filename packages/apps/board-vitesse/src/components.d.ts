e6627fa695f96ebbe910ddf65f70b7f9da22d770
Dup4 <lyuzhi.pan@gmail.com>
2023-08-08 14:09:13 +0800

    fix: method name error
    Signed-off-by: Dup4 <lyuzhi.pan@gmail.com>

--- packages/apps/board-vitesse/src/components.d.ts
+++ packages/apps/board-vitesse/src/components.d.ts
@@ -8,24 +8,16 @@ export {}
 declare module 'vue' {
   export interface GlobalComponents {
     Board: typeof import('./components/board/Board.vue')['default']
-    BoardEntry: typeof import('./components/BoardEntry.vue')['default']
     ContestIndex: typeof import('./components/ContestIndex.vue')['default']
-    copy: typeof import('./components/BoardEntry copy.vue')['default']
     Footer: typeof import('./components/Footer.vue')['default']
     GoBack: typeof import('./components/GoBack.vue')['default']
     NavBar: typeof import('./components/NavBar.vue')['default']
-    README: typeof import('./components/README.md')['default']
-    RightArrow: typeof import('./components/icon/RightArrow.vue')['default']
     RightArrowIcon: typeof import('./components/icon/RightArrowIcon.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
     Standings: typeof import('./components/board/Standings.vue')['default']
-    Team: typeof import('./components/Team.vue')['default']
     TeamUI: typeof import('./components/board/TeamUI.vue')['default']
-    TFooter: typeof import('./components/TFooter.vue')['default']
     TheCounter: typeof import('./components/TheCounter.vue')['default']
-    TheFooter: typeof import('./components/TheFooter.vue')['default']
     TheInput: typeof import('./components/TheInput.vue')['default']
-    TheNavBar: typeof import('./components/TheNavBar.vue')['default']
   }
 }
