339bf13b061eb4e29d807a78f5ef49411fed1c9e
Dup4 <lyuzhi.pan@gmail.com>
2023-08-09 09:36:06 +0800

    chore: add docker_entry script
    Signed-off-by: Dup4 <lyuzhi.pan@gmail.com>

---
+++ packages/apps/board-vitesse/docker/docker_entry.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+CUR_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
+
+BOARD_PATH="${CUR_DIR}/../dist"
+EXPORT_PATH="/app/export"
+
+bash "${CUR_DIR}/../scripts/inject_vars_all.sh"
+
+if [[ -d "${EXPORT_PATH}" ]]; then
+    rm -rf "${EXPORT_PATH:?}"/*
+    cp -a "${BOARD_PATH}"/* "${EXPORT_PATH}"/
+fi
+
+if [[ X"${1}" = X"primary" ]]; then
+    cd "${CUR_DIR}/.." || exit 1
+    exec npm run start
+else
+    exec "${@}"
+fi
