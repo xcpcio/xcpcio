168286e8249d24152d94d915822dd94b41666239
Dup4 <lyuzhi.pan@gmail.com>
2023-08-16 10:00:51 +0800

    style: format
    Signed-off-by: Dup4 <lyuzhi.pan@gmail.com>

--- packages/apps/board-vitesse/src/components/board/Statistics.vue
+++ packages/apps/board-vitesse/src/components/board/Statistics.vue
@@ -11,17 +11,25 @@ const rank = reactive(props.rank);
 </script>
 
 <template>
-  <div class="flex flex-col w-full mt-8 gap-8">
+  <div
+    class="flex flex-col w-full mt-8 gap-8"
+  >
     <div>
-      <Chart :options="getSubmitChart(rank)" />
+      <Chart
+        :options="getSubmitChart(rank)"
+      />
     </div>
 
     <div>
-      <Chart :options="getProblemChart(rank)" />
+      <Chart
+        :options="getProblemChart(rank)"
+      />
     </div>
 
     <div>
-      <Chart :options="getTeamChart(rank)" />
+      <Chart
+        :options="getTeamChart(rank)"
+      />
     </div>
   </div>
 </template>
