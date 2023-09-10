4d3b8f0267a0de70e3af0dbd803131c017566855
Dup4 <lyuzhi.pan@gmail.com>
2023-09-10 13:24:01 +0800

    style: format
    Signed-off-by: Dup4 <lyuzhi.pan@gmail.com>

--- packages/apps/board-vitesse/src/components/Footer.vue
+++ packages/apps/board-vitesse/src/components/Footer.vue
@@ -5,27 +5,43 @@ import { GITHUB_URL } from "@xcpcio/types";
 <template>
   <footer
     mt-8 p-4 lg:p-10 md:p-8
-    w-screen flex justify-center
+    w-screen
+    flex justify-center items-center
   >
-    <div class="mx-auto max-w-screen-lg text-center">
+    <div
+      mx-auto max-w-screen-lg text-center
+    >
       <RouterLink
-        class="flex items-center justify-center text-2xl font-semibold text-gray-900 dark:text-white"
+        flex items-center justify-center
+        text-2xl font-semibold text-gray-900 dark:text-white
         to="/"
       >
         <div
           i-ion-balloon-sharp
-          class="mr-4 h-8"
+          mr-4 h-8
         />
         Get more balloons
       </RouterLink>
 
-      <p class="my-6 text-gray-500 dark:text-gray-400">
+      <p
+        my-6 text-gray-500 dark:text-gray-400
+        w-full
+        flex justify-center
+      >
         Open-source project of ICPC/CCPC Standings.
       </p>
 
-      <ul class="mb-6 flex flex-wrap items-center justify-center text-gray-900 dark:text-white">
+      <ul
+        mb-6
+        w-full
+        flex flex-wrap items-center justify-center
+        text-gray-900 dark:text-white
+      >
         <li>
-          <RouterLink to="/" class="mr-4 md:mr-6 hover:underline">
+          <RouterLink
+            to="/"
+            mr-4 md:mr-6 hover:underline
+          >
             Home
           </RouterLink>
         </li>
@@ -51,7 +67,9 @@ import { GITHUB_URL } from "@xcpcio/types";
       </ul>
 
       <span
-        class="text-sm text-gray-500 sm:text-center dark:text-gray-400"
+        text-sm text-gray-500 sm:text-center dark:text-gray-400
+        w-full
+        flex justify-center
       >© 2020-PRESENT <RouterLink
         to="/"
         class="hover:underline"
