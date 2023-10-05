589a90d0936d87c2a7df61833e0d1034d4ef20cb
Dup4 <lyuzhi.pan@gmail.com>
2023-10-05 10:04:04 +0800

    chore: install xlsx
    Signed-off-by: Dup4 <lyuzhi.pan@gmail.com>

--- packages/libs/core/src/export/general-excel.ts
+++ packages/libs/core/src/export/general-excel.ts
@@ -1,3 +1,16 @@
+import * as XLSX from "xlsx";
+
+import type { Rank } from "../rank";
+
 export class GeneralExcelConverter {
   constructor() {}
+
+  public convert(_rank: Rank): XLSX.WorkBook {
+    const workbook = XLSX.utils.book_new();
+    return workbook;
+  }
+
+  public convertAndWrite(rank: Rank, filename: string): any {
+    return XLSX.writeFile(this.convert(rank), filename, { compression: true });
+  }
 }
