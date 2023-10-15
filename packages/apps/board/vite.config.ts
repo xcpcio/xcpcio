2921affe9d1aa15e68127499ba700ad2d5ca81ba
Dup4 <lyuzhi.pan@gmail.com>
2023-10-15 13:41:15 +0800

    chore: test
    Signed-off-by: Dup4 <lyuzhi.pan@gmail.com>

--- packages/apps/board/vite.config.ts
+++ packages/apps/board/vite.config.ts
@@ -103,17 +103,17 @@ export default defineConfig({
         theme_color: "#ffffff",
         icons: [
           {
-            src: "/balloon-192x192.png",
+            src: "/pwa-192x192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "/balloon-512x512.png",
+            src: "/pwa-512x512.png",
             sizes: "512x512",
             type: "image/png",
           },
           {
-            src: "/balloon-512x512.png",
+            src: "/pwa-512x512.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "any maskable",
