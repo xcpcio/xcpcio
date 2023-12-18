7473f46af3c8a5051b8c9f1c88f2f48aca477b51
Dup4 <lyuzhi.pan@gmail.com>
2023-12-18 23:45:57 +0800

    chore: optimize release progress
    Signed-off-by: Dup4 <lyuzhi.pan@gmail.com>

---
+++ packages/apps/board/scripts/replace_jsdelivr_cdn_host.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+CUR_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
+
+VERSION=$(cat <"${CUR_DIR}/../package.json" | grep version | sed 's|  \"version\": \"||g' | sed 's|\",||g')
+
+echo "$VERSION"
+
+export BOARD_CDN_HOST="https://cdn.jsdelivr.net/npm/@xcpcio/board-app@${VERSION}/dist"
+
+bash "${CUR_DIR}/inject_vars_all.sh"
