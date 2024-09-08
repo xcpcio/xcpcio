ff2facc2d2a8857351ce542d932e89d2ab88a7a6
Zxilly <zxilly@outlook.com>
2024-09-08 13:24:18 +0800

    feat: auto update sw.js

--- packages/apps/board/src/pwa.ts
+++ packages/apps/board/src/pwa.ts
@@ -1,3 +1,31 @@
 import { registerSW } from "virtual:pwa-register";
 
-registerSW({ immediate: true });
+const intervalMS = 60 * 60 * 1000;
+
+registerSW({
+  immediate: true,
+  onRegisteredSW(swUrl, r) {
+    r
+    && setInterval(async () => {
+      if (r.installing || !navigator) {
+        return;
+      }
+
+      if ("connection" in navigator && !navigator.onLine) {
+        return;
+      }
+
+      const resp = await fetch(swUrl, {
+        cache: "no-store",
+        headers: {
+          "cache": "no-store",
+          "cache-control": "no-cache",
+        },
+      });
+
+      if (resp?.status === 200) {
+        await r.update();
+      }
+    }, intervalMS);
+  },
+});
