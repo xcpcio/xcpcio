c0d9409cd1972566ddd07694802ddf4cabedf961
Frex <frexcheat@163.com>
2025-03-18 11:03:57 +0800

    refactor: heatmap data generate

--- packages/apps/board/src/components/board/SubmitHeatMap.vue
+++ packages/apps/board/src/components/board/SubmitHeatMap.vue
@@ -6,46 +6,135 @@ const props = defineProps<{
   rank: Rank;
 }>();
 
+const mapTimeDiffLength = 20;
 const rank = computed(() => props.rank);
 const startTime = rank.value.contest.getStartTime();
 const endTime = rank.value.contest.getEndTime();
 const totalDuration = endTime.diff(startTime, "minute");
-const intervalSize = totalDuration / 20;
+const intervalSize = totalDuration / mapTimeDiffLength;
+
+const correctStatus = [
+  SubmissionStatus.ACCEPTED,
+  SubmissionStatus.CORRECT,
+];
+
+const incorrectStatus = [
+  SubmissionStatus.REJECTED,
+  SubmissionStatus.WRONG_ANSWER,
+  SubmissionStatus.PARTIALLY_CORRECT,
+  SubmissionStatus.PRESENTATION_ERROR,
+  SubmissionStatus.MEMORY_LIMIT_EXCEEDED,
+  SubmissionStatus.TIME_LIMIT_EXCEEDED,
+  SubmissionStatus.OUTPUT_LIMIT_EXCEEDED,
+  SubmissionStatus.RUNTIME_ERROR,
+];
 
 const submissions = computed(() => rank.value.getSubmissions());
-const accepted = computed(() => submissions.value.filter(s => s.status === SubmissionStatus.ACCEPTED));
-const rejected = computed(() => submissions.value.filter(s => s.status === SubmissionStatus.WRONG_ANSWER));
+const submissionsByProblemId = computed(() => {
+  const result = new Map();
+  for (const p of rank.value.contest.problems) {
+    result.set(p.id, {
+      accepted: submissions.value.filter(s => s.problemId === p.id && correctStatus.includes(s.status)),
+      rejected: submissions.value.filter(s => s.problemId === p.id && incorrectStatus.includes(s.status)),
+    });
+  }
+  return result;
+});
 
-function getHeatMapData() {
+const intervalDescriptions = computed(() => {
+  return Array.from({ length: mapTimeDiffLength }, (_, index) => {
+    const start = index * intervalSize;
+    const end = (index + 1) * intervalSize;
+    return `Between ${start} and ${end} minutes: `;
+  });
+});
+
+const heatMapData = computed(() => {
   const h = [];
 
   for (const p of rank.value.contest.problems) {
-    const acSubmits = accepted.value.filter(s => s.problemId === p.id);
-    const waSubmits = rejected.value.filter(s => s.problemId === p.id);
-    const acHeatMap = Array.from({ length: 20 }, () => ({ count: 0, description: "" }));
-    const waHeatMap = Array.from({ length: 20 }, () => ({ count: 0, description: "" }));
+    const problemSubmissions = submissionsByProblemId.value.get(p.id);
+    const acSubmits = problemSubmissions.accepted;
+    const waSubmits = problemSubmissions.rejected;
 
-    acHeatMap.forEach((a, index) => {
-      a.description = `Between ${index * intervalSize} and ${(index + 1) * intervalSize} minutes: 0 correct submissions`;
-    });
+    const acHeatMap = Array.from({ length: mapTimeDiffLength }, (_, index) => ({
+      count: 0,
+      level: 0,
+      description: `${intervalDescriptions.value[index]}0 correct submissions`,
+    }));
 
-    waHeatMap.forEach((w, index) => {
-      w.description = `Between ${index * intervalSize} and ${(index + 1) * intervalSize} minutes: 0 rejected submissions`;
-    });
+    const waHeatMap = Array.from({ length: mapTimeDiffLength }, (_, index) => ({
+      count: 0,
+      level: 0,
+      description: `${intervalDescriptions.value[index]}0 incorrect submissions`,
+    }));
 
     for (const s of acSubmits) {
       const duration = s.timestampToMinute;
-      const index = Math.min(Math.floor(duration / intervalSize), 19);
+      const index = Math.min(Math.floor(duration / intervalSize), mapTimeDiffLength - 1);
       acHeatMap[index].count++;
-      acHeatMap[index].description = `Between ${index * intervalSize} and ${(index + 1) * intervalSize} minutes: ${acHeatMap[index].count} correct submissions`;
     }
 
     for (const s of waSubmits) {
       const duration = s.timestampToMinute;
-      const index = Math.min(Math.floor(duration / intervalSize), 19);
+      const index = Math.min(Math.floor(duration / intervalSize), mapTimeDiffLength - 1);
       waHeatMap[index].count++;
-      waHeatMap[index].description = `Between ${index * intervalSize} and ${(index + 1) * intervalSize} minutes: ${waHeatMap[index].count} rejected submissions`;
     }
+
+    for (let i = 0; i < mapTimeDiffLength; i++) {
+      if (acHeatMap[i].count > 0) {
+        acHeatMap[i].description = `${intervalDescriptions.value[i] + acHeatMap[i].count} correct submissions`;
+      }
+    }
+
+    for (let i = 0; i < mapTimeDiffLength; i++) {
+      if (waHeatMap[i].count > 0) {
+        waHeatMap[i].description = `${intervalDescriptions.value[i] + waHeatMap[i].count} incorrect submissions`;
+      }
+    }
+
+    const sortedAcHeatMap = acHeatMap.slice().sort((a, b) => b.count - a.count);
+    const sortedWaHeatMap = waHeatMap.slice().sort((a, b) => b.count - a.count);
+
+    const heatLevelThresholds = computed(() => {
+      const correctLevelThresholds = [
+        0,
+        sortedAcHeatMap[Math.floor(mapTimeDiffLength * 0.2)].count,
+        sortedAcHeatMap[Math.floor(mapTimeDiffLength * 0.4)].count,
+        sortedAcHeatMap[Math.floor(mapTimeDiffLength * 0.6)].count,
+        sortedAcHeatMap[Math.floor(mapTimeDiffLength * 0.8)].count,
+      ];
+      const incorrectLevelThresholds = [
+        0,
+        sortedWaHeatMap[Math.floor(mapTimeDiffLength * 0.2)].count,
+        sortedWaHeatMap[Math.floor(mapTimeDiffLength * 0.4)].count,
+        sortedWaHeatMap[Math.floor(mapTimeDiffLength * 0.6)].count,
+        sortedWaHeatMap[Math.floor(mapTimeDiffLength * 0.8)].count,
+      ];
+      return { correctLevelThresholds, incorrectLevelThresholds };
+    });
+
+    function getHeatLevel(thresholds: number[], count: number) {
+      if (count <= thresholds[0]) {
+        return 0;
+      } else if (count <= thresholds[1]) {
+        return 1;
+      } else if (count <= thresholds[2]) {
+        return 2;
+      } else if (count <= thresholds[3]) {
+        return 3;
+      } else {
+        return 4;
+      }
+    }
+
+    acHeatMap.forEach((h) => {
+      h.level = getHeatLevel(heatLevelThresholds.value.correctLevelThresholds, h.count);
+    });
+
+    waHeatMap.forEach((h) => {
+      h.level = getHeatLevel(heatLevelThresholds.value.incorrectLevelThresholds, h.count);
+    });
 
     h.push({
       id: p.id,
@@ -56,21 +145,7 @@ function getHeatMapData() {
     });
   }
   return h;
-}
-
-function getHeatLevel(count: number) {
-  if (count === 0) {
-    return "0";
-  } else if (count <= 50) {
-    return "1";
-  } else if (count <= 100) {
-    return "2";
-  } else if (count <= 200) {
-    return "3";
-  } else {
-    return "4";
-  }
-}
+});
 </script>
 
 <template>
@@ -90,7 +165,7 @@ function getHeatLevel(count: number) {
       place-items-center
     >
       <div
-        v-for="heatMap in getHeatMapData()" :key="heatMap.label"
+        v-for="heatMap in heatMapData" :key="heatMap.label"
         flex flex-col
         items-center justify-center
         w-350px h-100px
@@ -113,12 +188,12 @@ function getHeatLevel(count: number) {
         <div>
           <div flex flex-row gap-1 mb-2>
             <Tooltip
-              v-for="index in 20" :key="`ac-${index}`"
+              v-for="(acItem, index) in heatMap.acHeatMap" :key="`ac-${index}`"
               w-inherit
             >
               <div
                 class="accept-heat"
-                :data-level="getHeatLevel(heatMap.acHeatMap[index - 1].count)"
+                :data-level="acItem.level"
                 size-12px
                 rounded-1
                 shadow
@@ -129,7 +204,7 @@ function getHeatLevel(count: number) {
                   flex
                 >
                   <div>
-                    {{ heatMap.acHeatMap[index - 1].description }}
+                    {{ acItem.description }}
                   </div>
                 </div>
               </template>
@@ -137,10 +212,10 @@ function getHeatLevel(count: number) {
           </div>
 
           <div flex flex-row gap-1>
-            <Tooltip v-for="index in 20" :key="`wa-${index}`" w-inherit>
+            <Tooltip v-for="(waItem, index) in heatMap.waHeatMap" :key="`wa-${index}`" w-inherit>
               <div
                 class="rejected-heat"
-                :data-level="getHeatLevel(heatMap.waHeatMap[index - 1].count)"
+                :data-level="waItem.level"
                 size-12px
                 rounded-1
                 shadow
@@ -151,7 +226,7 @@ function getHeatLevel(count: number) {
                   flex
                 >
                   <div>
-                    {{ heatMap.waHeatMap[index - 1].description }}
+                    {{ waItem.description }}
                   </div>
                 </div>
               </template>
