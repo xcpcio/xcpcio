efa08767e353c7a0fed77d92014d0854dd70485b
Dup4 <lyuzhi.pan@gmail.com>
2025-09-13 16:13:41 +0800

    fix: metadata in index.html
    Signed-off-by: Dup4 <lyuzhi.pan@gmail.com>

--- packages/apps/board/src/App.vue
+++ packages/apps/board/src/App.vue
@@ -6,14 +6,6 @@ useHead({
   title: "Board - XCPCIO",
   meta: [
     {
-      name: "description",
-      content: "XCPCIO-Board 主要收录 *CPC 系列竞赛的榜单。",
-    },
-    {
-      name: "keywords",
-      content: "icpc, ccpc, rank, board, standings",
-    },
-    {
       name: "theme-color",
       content: () => isDark.value ? "#00aba9" : "#ffffff",
     },
