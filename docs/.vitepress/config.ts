6d9322dad48e149cecaec3d95b7bce9a8ee2e216
Duplicate4 <lyuzhi.pan@gmail.com>
2025-09-15 09:52:16 +0800

    feat: introduce google tag manager (#328)
    Signed-off-by: Dup4 <lyuzhi.pan@gmail.com>

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -5,6 +5,7 @@ const ogUrl = hostname;
 const ogImage = `${ogUrl}og.png`;
 const title = "XCPCIO";
 const description = "The ICPC Series Competition Leaderboard Visualization Engine.";
+const googleTagId = "GTM-WHLF55BJ";
 
 export default defineConfig({
   title,
@@ -34,7 +35,23 @@ export default defineConfig({
     ["meta", { name: "twitter:card", content: "summary_large_image" }],
     ["meta", { name: "twitter:image", content: ogImage }],
     ["meta", { name: "twitter:url", content: ogUrl }],
+    [
+      "script",
+      {},
+      `(function (w, d, s, l, i) {
+        w[l] = w[l] || [];
+        w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
+        var f = d.getElementsByTagName(s)[0],
+          j = d.createElement(s),
+          dl = l != "dataLayer" ? "&l=" + l : "";
+        j.async = true;
+        j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
+        f.parentNode.insertBefore(j, f);
+      })(window, document, "script", "dataLayer", "${googleTagId}");`,
+    ],
   ],
+
+  // TODO: add noscript google tag frame
 
   locales: {
     root: { label: "English", lang: "en-US", dir: "ltr" },
