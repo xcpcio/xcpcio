13a3a0b71c0fe0768fb831c49219a36eaedf9b34
Dup4 <lyuzhi.pan@gmail.com>
2025-12-04 21:37:52 +0800

    fix: banner load from relative path
    Signed-off-by: Dup4 <lyuzhi.pan@gmail.com>

--- packages/libs/core/src/utils/index.ts
+++ packages/libs/core/src/utils/index.ts
@@ -1,3 +1,10 @@
 export * from "./calc";
 export * from "./color";
 export * from "./dayjs";
+
+export function normalizePath(path: string) {
+  while (path.endsWith("/")) {
+    path = path.slice(0, -1);
+  }
+  return `${path}/`;
+}
