2192c9f407f583eafb83758f58a22a0d4736b2fb
Dup4 <lyuzhi.pan@gmail.com>
2026-01-15 18:52:11 +0800

    fix: favicon
    Signed-off-by: Dup4 <lyuzhi.pan@gmail.com>

--- packages/apps/board/src/App.vue
+++ packages/apps/board/src/App.vue
@@ -10,13 +10,6 @@ useHead({
       content: () => isDark.value ? "#00aba9" : "#ffffff",
     },
   ],
-  link: [
-    {
-      rel: "icon",
-      type: "image/svg+xml",
-      href: () => preferredDark.value ? "/favicon-dark.svg" : "/favicon.svg",
-    },
-  ],
 });
 </script>
 
