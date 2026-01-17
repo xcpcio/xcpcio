fcde364aa86e82b8bf7f3c4788f28bb6b6aabd14
Duplicate4 <lyuzhi.pan@gmail.com>
2026-01-17 22:13:29 +0800

    feat: introduce night rank (#515)
    Signed-off-by: Dup4 <lyuzhi.pan@gmail.com>

--- packages/apps/board/src/components/board/Standings.vue
+++ packages/apps/board/src/components/board/Standings.vue
@@ -158,7 +158,7 @@ const maxTeamLength = computed(() => {
     <div>
       <table
         class="standings"
-        font-mono dark:text-gray-700
+        font-mono dark:text-gray-200
       >
         <thead
           class="sticky top-0 z-99"
