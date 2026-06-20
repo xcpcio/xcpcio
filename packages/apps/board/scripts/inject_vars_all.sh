51aa08195a7c862fe74fa985c6cebf0b477f7c9c
Duplicate4 <lyuzhi.pan@gmail.com>
2026-06-20 21:28:19 +0800

    fix: inject vars script (#542)
    Signed-off-by: Dup4 <lyuzhi.pan@gmail.com>

--- packages/apps/board/scripts/inject_vars_all.sh
+++ packages/apps/board/scripts/inject_vars_all.sh
@@ -2,7 +2,7 @@
 
 CUR_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 
-while IFS= read -r -d '' file; do
+find "${CUR_DIR}/../dist" -type f \( -name "*.html" -o -name "*.js" \) -print0 | while IFS= read -r -d '' file; do
   echo "${file}"
   bash "${CUR_DIR}/inject_vars.sh" "${file}"
-done < <(find "${CUR_DIR}/../dist" -type f \( -name "*.html" -o -name "*.js" \) -print0)
+done
